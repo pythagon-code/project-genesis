4909423f99eb77c2eb3675714b348dbea8fe892f
Abhay Pokhriyal <abhaypokh89@gmail.com>
2025-12-13 17:41:38 -0600

    Update

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,11 +3,14 @@ import numpy as np
 import os
 import random
 import torch
-from torch import nn, optim
+from torch import optim
+from torch.nn.functional import mse_loss
+from tqdm import trange
 
 from maci.ml.fnn import FNN
 from maci.utils.mmap_object import MMapObject
 from maci.utils.model_optimizer import ModelOptimizer
+
 
 def test_mmap_object():
     os.makedirs("temp/mmap_test/", exist_ok=True)
@@ -37,8 +40,11 @@ def test_mmap_object():
 
 
 def test_model_optimizer():
+    os.makedirs("temp/model_optimizer_test/", exist_ok=True)
     for i in range(5):
         if os.path.exists(f"temp/model_optimizer_test/model{i}.bin"):
+            os.remove(f"temp/model_optimizer_test/model{i}.bin")
+        if os.path.exists(f"temp/model_optimizer_test/opt{i}.bin"):
             os.remove(f"temp/model_optimizer_test/opt{i}.bin")
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -48,7 +54,7 @@ def test_model_optimizer():
     for param in fnn2.parameters():
         param.requires_grad_(False)
 
-    opt = optim.Adam(fnn1.parameters(), lr=1e-3)
+    opt = optim.Adam(fnn1.parameters(), lr=0.1)
     fnn1(torch.randn(10, 10, device=device)).sum().backward()
     opt.step()
     opt.zero_grad(set_to_none=True)
@@ -63,13 +69,13 @@ def test_model_optimizer():
         model_opt_containers
     ) for i in range(5)]
 
-    for _ in range(300):
+    for _ in trange(2000):
         for i, model_opt in enumerate(model_opts):
             model, opt = model_opt.load_dirty()
             for param in model.parameters():
                 assert param.requires_grad
             x = torch.randn(32, 10, device=device)
-            loss = (torch.sum(model(x)) - i) ** 2
+            loss = mse_loss(model(x), i * torch.ones_like(x))
             loss.backward()
             opt.step()
             opt.zero_grad(set_to_none=True)
@@ -77,7 +83,8 @@ def test_model_optimizer():
 
     for i, model_opt in enumerate(model_opts):
         model = model_opt.load_frozen()
-        y = torch.sum(model(torch.randn(32, 10, device=device)))
-        error = abs(y.item() - i)
-        assert error < 0.1
+        x = torch.randn(32, 10, device=device)
+        loss = mse_loss(model(x), i * torch.ones_like(x))
+        print(loss.item())
+        assert loss.item() < 0.01
         model_opt.unload_frozen(model)
